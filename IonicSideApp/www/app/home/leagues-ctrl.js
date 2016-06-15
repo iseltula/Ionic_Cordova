e6346e19ebbfb9b3e725c9fe9d0a8e142b5f9ddf
Isel Tula <iseltula@Isels-MacBook-Pro.local>
2016-06-15 00:17:38 -0400

    Ionic App. Full base

---
+++ IonicSideApp/www/app/home/leagues-ctrl.js
@@ -0,0 +1,15 @@
+(function(){
+    'use strict';
+    angular.module('eliteApp').controller('LeaguesCtrl', ['$state','eliteApi', LeaguesCtrl]);
+
+    function LeaguesCtrl($state, eliteApi){
+        var vm = this;
+        
+        var leagues = eliteApi.getLeagues();
+        vm.leagues=leagues;
+
+        vm.selectedLeague = function(leagueId){
+            $state.go("app.teams");
+        }
+    };
+})();
